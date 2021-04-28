8f51cb0cfe939d3db83f20fd397676272abf1285
huangkaicheng <ronald_huang@foxmail.com>
2021-04-28 17:41:21 +0800

    init-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+/*
+ * @Description:
+ * @Author: huangkaicheng
+ * @Date: 2021-04-23 16:28:54
+ * @LastEditTime: 2021-04-23 16:34:13
+ * @LastEditors: huangkaicheng@foxmail.com
+ */
+import "./assets/css/App.css";
+
+function App() {
+  return <div>App...</div>;
+}
+
+export default App;
